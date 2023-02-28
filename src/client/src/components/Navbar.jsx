f7cd0039e87f52c9860cae44d1c2d7f82cd7cf2f
JordanWalling <jordanwalling1991@gmail.com>
2023-03-01 09:16:51 +1100

    reorganise navbar with styled components

---
+++ src/client/src/components/Navbar.jsx
@@ -0,0 +1,87 @@
+import { NavLink, Link } from "react-router-dom";
+import classes from "./Navbar.module.css";
+import vdp_logo from "../assets/logo.png";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background: #cddeee;
+  justify-content: center;
+  height: 3rem;
+  display: flex;
+  position: sticky;
+  z-index: 9;
+`;
+const NavLeft = styled.div`
+  flex: 2;
+`;
+const NavRight = styled.div`
+  flex: 5;
+`;
+const NavList = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+const NavItems = styled.div`
+  list-style-type: none;
+  padding-top: 0.75rem;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <NavLeft>
+        <Link to="/">
+          <img
+            className={classes.navbarImage}
+            src={vdp_logo}
+            alt="van dyke logo"
+          />
+        </Link>
+      </NavLeft>
+      <NavRight>
+        <NavList>
+          <NavItems>
+            <NavLink className={classes.navLink} to="contact-us">
+              Contact Us
+            </NavLink>
+          </NavItems>
+          <NavItems>
+            <NavLink className={classes.navLink} to="blogs">
+              Blogs
+            </NavLink>
+          </NavItems>
+          <NavItems>
+            <NavLink className={classes.navLink} to="login">
+              Login
+            </NavLink>
+          </NavItems>
+          <NavItems>
+            <NavLink className={classes.navLink} to="blogs/:id">
+              Edit Blog
+            </NavLink>
+          </NavItems>
+          <NavItems>
+            <NavLink className={classes.navLink} to="jobs">
+              Jobs
+            </NavLink>
+          </NavItems>
+        </NavList>
+      </NavRight>
+    </Container>
+  );
+};
+export default Navbar;
+
+//<div className={classes.container}>
+//       <div className={classes.navLeft}>
+//         <Link className={classes.navLink} to="/">
+//           <img src={vdp_logo} alt="van dyke logo" />
+//         </Link>
+//       </div>
+//       <div className={classes.navRight}>
+
+//       </div>
+//     </div>
+//   );
+// };
