38c9031b3820e83e83febd587e3a59d17fd3ba45
JordanWalling <jordanwalling1991@gmail.com>
2023-03-24 11:26:23 +1100

    alter contact page styling

--- src/client/src/components/ContactUsStyle.jsx
+++ src/client/src/components/ContactUsStyle.jsx
@@ -1,9 +1,19 @@
 import styled from "styled-components";
 
 export const ContactContainer = styled.div`
-  height: 140vh;
+  height: 100%;
   background-color: #056ed7fa;
-  position: relative;
+  padding-bottom: 5rem;
+
+  /* display: flex;
+  justify-content: center;
+  align-items: center; */
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 export const Container = styled.div`
   /* padding-top: 5rem; */
@@ -12,11 +22,11 @@ export const Container = styled.div`
   align-content: center;
   justify-items: center;
 
-  width: 60vw;
-  position: absolute;
+  width: 50%;
+
   /* height: 80vh; */
-  left: 20%;
-  top: 25%;
+  /* left: 20%;
+  top: 25%; */
   border-radius: 10px;
   box-shadow: 1px 2px 3px solid #fff;
   max-width: 872px;
@@ -26,35 +36,37 @@ export const Container = styled.div`
 export const ContainerItem = styled.div`
   Iframe {
     width: 100%;
-    height: 25vh;
+    height: 20vh;
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
   }
 `;
 
 export const ContactUsHeading = styled.h1`
-  font-size: 10vw;
+  font-size: 10rem;
   display: flex;
   justify-content: center;
-  padding: 2vh 0;
+
   color: #fff;
 `;
 export const ContactUsForm = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  margin: auto;
   width: 100%;
   padding: 20px 5px;
   border-radius: 10px;
 `;
 
 export const FormItem = styled.div`
-  padding-bottom: 2vh;
+  /* padding-bottom:; */
   display: inline-block;
   input {
     width: 100%;
     height: 3vh;
-    font-size: 2vw;
+    font-size: 1.5rem;
+    padding-left: 0.5rem;
     :hover {
       background-color: #f1eeee;
       font-weight: 600;
@@ -62,8 +74,9 @@ export const FormItem = styled.div`
   }
   textarea {
     width: 100%;
-    height: 15vh;
-    font-size: 2vw;
+    height: 10rem;
+    font-size: 1.5rem;
+    padding-left: 0.5rem;
     :hover {
       background-color: #f1eeee;
       font-weight: 600;
@@ -73,13 +86,13 @@ export const FormItem = styled.div`
 export const Label = styled.label`
   font-weight: 800;
   padding-right: 2vh;
-  font-size: 3vw;
+  font-size: 1.75rem;
   margin: 0;
 `;
 export const SubHeading = styled.h3`
   font-weight: 800;
   padding-bottom: 3vh;
-  font-size: 5vw;
+  font-size: 3rem;
   color: #056ed7fa;
   margin: auto;
 `;
@@ -92,10 +105,12 @@ export const ContactUsButton = styled.button`
   align-content: center;
   justify-content: center;
   align-items: center;
-  font-size: 2vw;
+  font-size: 1.5rem;
   background-color: #056ed7fa;
   color: #fff;
   border-radius: 5px;
+  text-transform: uppercase;
+  letter-spacing: 0.25rem;
   :hover {
     background-color: #fff;
     color: #056ed7fa;
--- src/client/src/pages/ContactUs.js
+++ src/client/src/pages/ContactUs.js
@@ -12,6 +12,7 @@ import {
   Label,
   SubHeading,
   ContactUsButton,
+  Wrapper,
 } from "../components/ContactUsStyle.jsx";
 import Banner from "../components/dashboard/Banner";
 
@@ -47,60 +48,64 @@ const ContactUs = () => {
       <Banner>
         <ContactUsHeading>Contact Us</ContactUsHeading>
       </Banner>
-      <Container>
-        <ContainerItem>
-          <Iframe
-            url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d923.5267687746074!2d151.27091612019757!3d-33.76899836476214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12aa5b982b3543%3A0x42c313a9f25fc931!2s8%20Short%20St%2C%20Brookvale%20NSW%202100!5e0!3m2!1sen!2sau!4v1678486291314!5m2!1sen!2sau"
-            width="100%"
-            height="100%"
-            style="border:0;"
-            allowFullScreen=""
-            loading="lazy"
-            content="no-referrer-when-downgrade"
-          />
-        </ContainerItem>
-        <ContactUsForm onSubmit={handleEmailSubmit}>
-          <SubHeading>Please provide your details</SubHeading>
-          <FormItem>
-            <Label htmlFor="name">Name:</Label>
-            <input
-              type="text"
-              autoFocus
-              value={name}
-              placeholder="Please enter your name"
-              onChange={(e) => setName(e.target.value)}
+      <Wrapper>
+        <Container>
+          <ContainerItem>
+            <Iframe
+              url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d923.5267687746074!2d151.27091612019757!3d-33.76899836476214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12aa5b982b3543%3A0x42c313a9f25fc931!2s8%20Short%20St%2C%20Brookvale%20NSW%202100!5e0!3m2!1sen!2sau!4v1678486291314!5m2!1sen!2sau"
+              width="100%"
+              height="100%"
+              style="border:0;"
+              allowFullScreen=""
+              loading="lazy"
+              content="no-referrer-when-downgrade"
             />
-          </FormItem>
-          <FormItem>
-            <Label htmlFor="phone">Phone Number:</Label>
-            <input
-              type="number"
-              value={phoneNumber}
-              placeholder="Please enter your Phone Number"
-              onChange={(e) => setPhoneNumber(e.target.value)}
-            />
-          </FormItem>
-          <FormItem>
-            <Label htmlFor="email">Email:</Label>
-            <input
-              type="email"
-              value={userEmail}
-              placeholder="Please enter your email address"
-              onChange={(e) => setUserEmail(e.target.value)}
-            />
-          </FormItem>
-          <FormItem>
-            <Label>Enquiry:</Label>
-            <textarea
-              type="text"
-              value={message}
-              placeholder="Please type any enquiries"
-              onChange={(e) => setMessage(e.target.value)}
-            />
-          </FormItem>
-          <ContactUsButton onSubmit={handleEmailSubmit}>Submit</ContactUsButton>
-        </ContactUsForm>
-      </Container>
+          </ContainerItem>
+          <ContactUsForm onSubmit={handleEmailSubmit}>
+            <SubHeading>Please provide your details</SubHeading>
+            <FormItem>
+              <Label htmlFor="name">Name:</Label>
+              <input
+                type="text"
+                autoFocus
+                value={name}
+                placeholder="Please enter your name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </FormItem>
+            <FormItem>
+              <Label htmlFor="phone">Phone Number:</Label>
+              <input
+                type="number"
+                value={phoneNumber}
+                placeholder="Please enter your Phone Number"
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+            </FormItem>
+            <FormItem>
+              <Label htmlFor="email">Email:</Label>
+              <input
+                type="email"
+                value={userEmail}
+                placeholder="Please enter your email address"
+                onChange={(e) => setUserEmail(e.target.value)}
+              />
+            </FormItem>
+            <FormItem>
+              <Label>Enquiry:</Label>
+              <textarea
+                type="text"
+                value={message}
+                placeholder="Please type any enquiries"
+                onChange={(e) => setMessage(e.target.value)}
+              />
+            </FormItem>
+            <ContactUsButton onSubmit={handleEmailSubmit}>
+              Submit
+            </ContactUsButton>
+          </ContactUsForm>
+        </Container>
+      </Wrapper>
     </ContactContainer>
   );
 };
