13b486426ea6f1b39299c91600d37eb672ca5db7
JordanWalling <jordanwalling1991@gmail.com>
2023-03-25 14:23:58 +1100

    configure deployment variables

--- src/server/index.js
+++ src/server/index.js
@@ -7,19 +7,22 @@ const userRoutes = require("./routes/user");
 const emailRoutes = require("./routes/email");
 
 const morgan = require("morgan");
+const helmet = require("helmet");
+
 require("dotenv").config();
 
 const app = express();
 
-// const corsOptions = {
-//   origin: process.env.REACT_DEV_PORT,
-//   optionsSuccessStatus: 200,
-// };
+const corsOption = {
+  origin: [process.env.REACT_DEV_PORT],
+  optionsSuccessStatus: 200,
+};
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOption));
 app.use(morgan("dev"));
+app.use(helmet());
 
 // route middleware
 app.use("/api", jobRoutes);
@@ -27,11 +30,11 @@ app.use("/api", blogRoutes);
 app.use("/api", userRoutes);
 app.use("/api", emailRoutes);
 
-port = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 // Database Connection and Port
 mongoose.connect(process.env.MONGO_URI).then(() => {
   app.listen(process.env.PORT, () => {
-    console.log(`MongoDb is connected, Listening on port ${port} `);
+    console.log(`MongoDb is connected, Listening on port ${PORT} `);
   });
 });
